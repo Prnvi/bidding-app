dd21b61931e2d5a07231263e6c0743770e3f3196
Pranavi <pranavi.l93@gmail.com>
2023-03-22 21:15:41 +0100

    create app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Tailwind for Bidding app!
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
